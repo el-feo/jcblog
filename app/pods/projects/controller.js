7ea71f3b19f1127da29b26126446b31e3e123919
el-feo <jebcoleman@gmail.com>
2015-06-29 16:07:28 -0700

    added bourbon, reworked project structure, and updated projects

---
+++ app/pods/projects/controller.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  transparencyImg: "/img/The_Transparency_Project.jpg",
+  prodsenseImg: "/img/Prod__Sense___P_O_s.jpg",
+  visualCortexImg: "/img/tam-visual_cortex.jpg",
+  brainImg: "/img/THE_BRAIN_FULL.jpg",
+  sceneImg: "/img/AFS-PRODUCTION_MODULE.jpg"
+});
